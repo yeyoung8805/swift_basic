2932869f71fa4e369f82f0f8fbe22dfdcba3c54c
“yeyoung8805” <yyk8805@naver.com>
2023-03-05 11:31:26 +0900

    [명언 생성 기능 구현하기 -2] Main.storyboard 사용, 랜덤함수 사용

---
+++ ch01_well-knownSaying/Quote.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+struct Quote {
+  let contents: String
+  let name: String
+}
--- ch01_well-knownSaying/ViewController.swift
+++ ch01_well-knownSaying/ViewController.swift
@@ -1,14 +1,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-  @IBOutlet weak var colorView: UIView!
+  @IBOutlet weak var quoteLabel: UILabel!
+  @IBOutlet weak var nameLabel: UILabel!
+  let quotes = [
+    Quote(contents: "죽음은 두려워하는 나머지 삶을 시작조차 못하는 사람이 많다.", name: "벤다이크")
+    Quote(contents: "나는 나 자신을 빼놓고는 모두 안다.", name: "비용")
+    Quote(contents: "편견이란 실효성이 없는 의견이다.", name: "암브로스 빌")
+    Quote(contents: "분노는 바보들의 가슴속에서만 살아간다.", name: "아인슈타인")
+    Quote(contents: "몇 번이라도 좋다! 이 끔찍한 생이여... 다시!", name: "니체")
+  ]
 
   override func viewDidLoad() {
     super.viewDidLoad()
   }
 
-  @IBAction func tabChangeColorButton(_ sender: UIButton) {
-    self.colorView.backgroundColor = UIColor.blue
-    print("색상 변경 버튼이 클릭되었음")
+  @IBAction func tabQuoteGeneratorButton(_ sender: Any) {
+    let random = Int(arc4random_uniform(5)) //0 ~ 4 사이의 난수를 랜덤하게 만들어 준다.
+    let quote = quotes[random]
+    self.quoteLabel.text = quote.contents
+    self.nameLabel.text = quote.name
   }
 }
