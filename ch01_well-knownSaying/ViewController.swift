cfea33b7644e681f1964f0661fc287943a0e31a0
“yeyoung8805” <yyk8805@naver.com>
2023-03-05 11:01:23 +0900

    [명언생성] IBOutlet & IBAction 알아보기 : 버튼 클릭에 따른 배경색상 변경

--- ch01_well-knownSaying/ViewController.swift
+++ ch01_well-knownSaying/ViewController.swift
@@ -6,4 +6,9 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
   }
+
+  @IBAction func tabChangeColorButton(_ sender: UIButton) {
+    self.colorView.backgroundColor = UIColor.blue
+    print("색상 변경 버튼이 클릭되었음")
+  }
 }
