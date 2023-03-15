cad77a4dd443e70633a05e0571a781163314e42e
yeyoung8805 <yyk8805@naver.com>
2023-03-15 17:21:49 +0900

    [ViewController Life cycle] print 출력,

--- ch02_LEDBoard/ViewController.swift
+++ ch02_LEDBoard/ViewController.swift
@@ -3,6 +3,27 @@ import UIKit
 class ViewController: UIViewController {
   override func viewDidLoad() {
     spuer.viewDidLoad()
+    print("ViewController 뷰가 로드 되었다.")
+  }
+
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    print("ViewController 뷰가 나타날 것이다.")
+  }
+
+  override func viewDidAppear(_ animated: Bool) {
+    spuer.viewDidAppear(animated)
+    print("ViewController 뷰가 나타났다.")
+  }
+
+  override func viewWillDisappear(_ animated: Bool) {
+    spuer.viewWillDisappear(animated)
+    print("ViewController 뷰가 사라질 것이다.")
+  }
+
+  override func viewDidDisappear(_ animated: Bool) {
+    spuer.viewDidDisappear(animated)
+    print("ViewController 뷰가 사라졌다.")
   }
 
   @IBAction func tapCodePresentButton(_ sender: UIButton) {
