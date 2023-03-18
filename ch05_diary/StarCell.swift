0593345f6e817f2a50f11674e78e8a1827784589
“yeyoung8805” <yyk8805@naver.com>
2023-03-18 19:01:40 +0900

    [일기장 기본 UI 그리기-1] 일기장, 즐겨찾기 (컬렉션 뷰 연결)

---
+++ ch05_diary/StarCell.swift
@@ -0,0 +1,5 @@
+import UIKit
+
+class StarCell: UICollectionViewCell {
+  
+}
---
+++ ch05_diary/StarVuewController.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+class StarViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
