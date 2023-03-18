d7efa22087c0bd4e80054652bdfb4b501973a711
“yeyoung8805” <yyk8805@naver.com>
2023-03-18 16:08:56 +0900

    [계산기 UI 그리기-3] RoundButton으로 만들기

---
+++ ch03_calculator/RoundButton.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+@IBDesignable
+class RoundButton: UIButton {
+  @IBInspectable var isRound: Bool = false {
+    didSet {
+      if isRound {
+        self.layer.cornerRadius = self.frame.height / 2
+      }
+    }
+  }
+}
